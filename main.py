af7f11f9a1903965306e5da785bc078cd40b3489
kkkkkijun <seo930714@gmail.com>
2025-11-11 17:29:44 +0900

    Remove 'SUI-USD' from crypto tickers list

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pytz
 
 # 종목 리스트
 stock_tickers = ["NVDA", "TSLA", "HIMS", "OSCR"]
-crypto_tickers = ["BTC-USD", "ETH-USD", "SOL-USD", "SUI-USD"]
+crypto_tickers = ["BTC-USD", "ETH-USD", "SOL-USD"]
 
 # 수신자 이메일
 recipients = ["seo930714@gmail.com", "mjikshouse@naver.com"]
